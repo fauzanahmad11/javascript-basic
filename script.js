ab30f6e4a4e7f463ef8bdc94880fdf3b90a69898
Fauzan Ahmad <fauzanahmadgojeck@gmail.com>
2021-12-28 16:10:17 +0700

    tipe data number

---
+++ script.js
@@ -0,0 +1,65 @@
+// ==========Hello World Program==========
+console.log("Hello World");
+document.writeln("Hello World");
+// ==========Comment==========
+/*
+- //
+*/
+
+//  ========== Tipe Data Number ==========
+
+//  ========== Tipe Data Boolean ========== 
+//  ========== Tipe Data String ========== 
+//  ========== Variable ========== 
+//  ========== Operator Matematika ========== 
+//  ========== Operator Perbandingan ========== 
+//  ========== Operator Logika ========== 
+//  ========== Console ========== 
+//  ========== String Template ========== 
+//  ========== Konversi String dan Number ========== 
+//  ========== Tipe Data Array ========== 
+//  ========== Tipe Data Object ========== 
+//  ========== If Expression ========== 
+//  ========== Popup ========== 
+//  ========== Undefined ========== 
+//  ========== Null ========== 
+//  ========== Switch Expression ========== 
+//  ========== Operator typeof ========== 
+//  ========== Operator in ========== 
+//  ========== Ternary Operator ========== 
+//  ========== Nullish Coalescing Operator ========== 
+//  ========== Optional Chaining ========== 
+//  ========== Falsy dan Truthy ========== 
+//  ========== Operator Logika di Non Boolean ========== 
+//  ========== For Loop ========== 
+//  ========== While Loop ========== 
+//  ==========  Do While Loop ========== 
+//  ========== Break dan Continue ========== 
+//  ========== Label ========== 
+//  ========== For In ========== 
+//  ========== For Of ========== 
+//  ========== With Statement ========== 
+//  ========== Function ========== 
+//  ========== Function Parameter ========== 
+//  ========== Function Return Value ========== 
+//  ========== Optional Parameter ========== 
+//  ========== Default Parameter ========== 
+//  ========== Rest Parameter ========== 
+//  ========== Function Sebagai Value ========== 
+//  ========== Anonymous Function ========== 
+//  ========== Function dalam Function ========== 
+//  ========== Scope ========== 
+//  ========== Recursive Function ========== 
+//  ========== Function GEnerator ========== 
+//  ========== Recursive Function ========== 
+//  ========== Function GEnerator ========== 
+//  ========== Arrow Function ========== 
+//  ========== Closure ========== 
+//  ========== Object Method ========== 
+//  ========== Kata Kunci this ========== 
+//  ========== Arrow Function di object ========== 
+//  ========== Getter dan Setter ========== 
+//  ========== Masalah Variable var ========== 
+// ========== Destructuring ==========
+// ========== Strict Mode ==========
+// ========== Debugger ==========
